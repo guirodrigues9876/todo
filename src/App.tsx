80b03131a6c8858e5ec7fff59a1d86b9c8b42ccf
Guilherme Rodrigues <guilherme121225@gmail.com>
2023-11-28 23:10:54 -0300

    start

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import styles from './App.module.css';
+import './global.css';
+
+export function App() {
+  return (
+    <div className={styles.wrapper}>
+
+      <header >
+        <div>Oi</div>
+      </header>
+
+    </div>
+  )
+}
+
+export default App
