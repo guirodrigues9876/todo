e173c2fe0aee89d7005fc9635b00c557842747a6
Guilherme Rodrigues <guilherme121225@gmail.com>
2023-11-29 18:34:27 -0300

    feat: home

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import styles from './App.module.css';
 import './global.css';
+import Logo from './assets/Logo.png';
 
 export function App() {
   return (
     <div className={styles.wrapper}>
+      <header className={styles.header}>
+        <img src={Logo} alt="Logo" />
 
-      <header >
-        <div>Oi</div>
       </header>
+
+      
 
     </div>
   )
