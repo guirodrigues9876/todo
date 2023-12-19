7e4f02323d874ef5f77bed4bc6ca4217a595f44f
Guilherme Rodrigues <guilherme121225@gmail.com>
2023-12-19 13:54:51 -0300

    feat: add task and toogle check box

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,59 @@
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { PlusCircle } from '@phosphor-icons/react'
+
 import './global.css';
 import styles from './App.module.css';
 import Logo from './assets/Logo.png';
 
-import { NewTask } from './components/NewTask';
+import { Button } from './components/Button';
 import { Task } from './components/Task';
 import { Empty } from './components/Empty';
 
-import { v4 as uuidv4 } from 'uuid';
+import { Input } from './components/Input';
 
 export function App() {
+  const [tasks, setTasks] = useState([])
+  const [inputValue, setInputValue] = useState('')
 
-  const tasks = [
-    {
-      id: uuidv4(),
-      title: 'Terminar o desafio',
-      isChecked: false
-    },
-    { 
-      id: uuidv4(),
-      title: 'Estudar JS',
-      isChecked: true
-    },
-    { 
-      id: uuidv4(),
-      title: 'Fazer projeto TypeScript',
-      isChecked: true
+  function handleAddTask(){
+    
+    if(!inputValue){
+      return
     }
-  ]
+
+    const newTask = {
+      id: uuidv4(),
+      text: inputValue,
+      isChecked: false
+    }
+
+    setTasks((state) => [...state, newTask])
+    setInputValue('')
+  }
+
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  function handleToggleTask({ id, value }: { id: number; value: boolean }) {
+    console.log(value)
+    const updatedTasks = tasks.map((task) => {
+      if (task.id === id) {
+        console.log("oi")
+        return {...task, isChecked: value }
+      }
+      
+      return { ...task }
+    })
+    
+    setTasks(updatedTasks)
+    console.log("updated tasks")
+    console.log(updatedTasks)
+    sleep(20000)
+    console.log(tasks)
+    console.log(value)
+  }
 
   return (
     <div className={styles.wrapper}>
@@ -37,7 +64,20 @@ export function App() {
 
       <main className={styles.main}>
 
-        <NewTask />
+      <div className={styles.newTaskContainer}>
+        <Input
+          onChange = {(e) => setInputValue(e.target.value)}
+          value = {inputValue}
+        />
+        <Button
+          onClick={handleAddTask}
+        >
+          Criar
+          <PlusCircle size={24} color="#e8e8e8" weight="bold" />
+        </Button>
+
+      </div>
+
 
         <div className={styles.tasksDisplay}>
           <div className={styles.info}>
@@ -50,14 +90,15 @@ export function App() {
               <span>{`${tasks.filter(task => task.isChecked).length} de ${tasks.length}`}</span>
             </div>
           </div>
-
+          
           {tasks.length > 0 ? (
               <div>
                 {tasks.map(task => {
                     return(
                       <Task 
                         key= {task.id}
-                        task={task}
+                        data={task}
+                        toggleTaskStatus={handleToggleTask}
                       />
                     )
                   })}
